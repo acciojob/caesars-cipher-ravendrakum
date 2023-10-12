1adcd70ae171166f32b0152e960ccf22eb79f992
Ravendra kumar <111950764+ravendrakum@users.noreply.github.com>
2023-10-12 23:57:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -34,8 +34,20 @@ const lookup = {
 function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
+	 for (let i = 0; i < encodedStr.length; i++) 
+	 {
+		 const char = encodedStr[i];
+		 if (lookup.hasOwnProperty(char)) {
+			 decodedArr.push(lookup[char]);
+         } 
+		 else 
+		 {
+			 decodedArr.push(char);
+         }
+     }
 
-  return; //return decodedArr
+  return decodedArr.join(''); // Convert the array back to a string
+
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
